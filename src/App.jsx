5f084f925a51797fc3d5e926e09c7c078c1afd4c
Muhammad Farid <mfarid2121@gmail.com>
2023-04-23 23:29:26 +0700

    change bit content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import 'aos/dist/aos.css';
 import './css/style.css';
@@ -11,12 +7,8 @@ import './css/style.css';
 import AOS from 'aos';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
@@ -29,18 +21,15 @@ function App() {
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,27 +12,23 @@ import Banner from '../partials/Banner';
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
         <FeaturesBlocks />
         <Testimonials />
         <Newsletter />
-
       </main>
 
       <Banner />
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
