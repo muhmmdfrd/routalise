03dd949ef8d6f8dbc3a0a77adacd58320f3e6792
Muhammad Farid <mfarid2121@gmail.com>
2023-05-20 08:55:50 +0700

    add music

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/Features';
 import FeaturesBlocks from '../partials/FeaturesBlocks';
@@ -10,19 +10,14 @@ import Banner from '../partials/Banner';
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Page content */}
       <main className="flex-grow">
-        {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
         <FeaturesBlocks />
         <Testimonials />
         <Newsletter />
       </main>
-
       <Banner />
-
-      {/*  Site footer */}
       <Footer />
     </div>
   );
--- src/partials/Banner.jsx
+++ src/partials/Banner.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import song from '../audios/heaven.mp3';
+import ReactSound from 'react-sound';
 
 function Banner() {
+  const [play, setPlay] = React.useState(false);
+
   return (
     <>
       <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60">
         <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
           <div className="text-slate-500 inline-flex">
             <a
-              className="font-medium hover:underline text-emerald-400"
-              href="https://cruip.com/simple/"
-              target="_blank"
+              className="font-medium hover:underline text-emerald-400 flex justify-center items-center"
+              role="button"
               rel="noreferrer"
+              onClick={() => setPlay((p) => !p)}
             >
-              Turn off the music
+              <ReactSound
+                className={'animate__animated animate__shakeX'}
+                url={song}
+                loop={true}
+                volume={75}
+                playStatus={play ? 'PLAYING' : 'PAUSED'}
+              />
+              {play ? 'Turn Off The Music' : 'Turn On The Music'}
             </a>
           </div>
         </div>
